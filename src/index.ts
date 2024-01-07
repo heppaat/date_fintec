3083eb31e8bce59bdfca4c053f0af82d4e6f06bb
heppaat <pali.herrer@gmail.com>
2024-01-07 14:17:53 +0100

    ts data type defined

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,39 @@ let DAYS = [
   "Sunday",
 ];
 
+type Data2 = Record<
+  string,
+  {
+    division: string;
+    events: { title: string; date: string; notes: string; bunting: boolean }[];
+  }
+>;
+
+type Data = {
+  [property: string]: {
+    division: string;
+    events: { title: string; date: string; notes: string; bunting: boolean }[];
+  };
+};
+
+const printData = (data: Data) => {
+  for (const key in data) {
+    console.log(data[key]);
+  }
+};
+
+const readData = async () => {
+  try {
+    const input = await readFile(`${__dirname}/../data.json`, "utf-8");
+    const jsonData = JSON.parse(input);
+    printData(jsonData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+readData();
+
 // DON'T MODIFY THE CODE BELOW THIS LINE
 
 let toExport;
